06b87b18f8670b8ac6933fcfb10c723ebeb7dc81
markvincentricarde <vincentricarde@gmail.com>
2024-09-23 18:36:19 +0800

    initial commit

---
+++ activity.js
@@ -0,0 +1,14 @@
+// variable
+
+let Name = "Mark";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "male";
+let facebook = "Mark Vincent Ricarde";
+
+//print output
+
+console.log(Name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(facebook);
+
